2d5842630ef5ae0bf867af546822f7abad892a30
Симизар <simizarmirzabekova@gmail.com>
2026-06-28 13:12:34 +0400

    Update lesson_3_task_2.py

--- lesson_3_task_2.py
+++ lesson_3_task_2.py
@@ -3,7 +3,6 @@ from smartphone import Smartphone
 catalog = [
     Smartphone(brand="Apple", model="iPhone 15 Pro", phone_number="+79123456789"),
     Smartphone(brand="Samsung", model="Galaxy S23 Ultra", phone_number="+79876543210"),
-    ...
 ]
 
 for phone in catalog:

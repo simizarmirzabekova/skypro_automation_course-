d7e139b660c01a39262ce3ae56719c696e0c7c58
Симизар <simizarmirzabekova@gmail.com>
2026-07-30 14:57:00 +0400

    описание изменений

--- test_saucedemo_and_calculator.py
+++ test_saucedemo_and_calculator.py
@@ -2,27 +2,11 @@ import time
 
 import pytest
 
-from selenium import webdriver
-
-from selenium.webdriver.common.by import By
-
-# Импортируем наши новые классы страниц
-from pages.calculator_page import CalculatorPage
-from pages.login_page import LoginPage
-from pages.shop_cart_page import ShopCartPage
-from pages.shop_checkout_page import ShopCheckoutPage
-from pages.shop_main_page import ShopMainPage
-
-
-import time
-import pytest
-from selenium import webdriver
-# Импортируем наши новые классы страниц
-from ..pages.calculator_page import CalculatorPage
-from ..pages.login_page import LoginPage
-from ..pages.shop_cart_page import ShopCartPage
-from ..pages.shop_checkout_page import ShopCheckoutPage
-from ..pages.shop_main_page import ShopMainPage
+from ..lesson7.pages.calculator_page import CalculatorPage 
+from ..lesson7.pages.login_page import LoginPage          
+from ..lesson7.pages.shop_cart_page import ShopCartPage  
+from ..lesson7.pages.shop_checkout_page import ShopCheckoutPage  
+from ..lesson7.pages.shop_main_page import ShopMainPage  
 
 
 @pytest.fixture(scope='function')
@@ -33,8 +17,10 @@ def chrome_driver():
         yield driver
         driver.quit()
 
+
 def test_calculator_with_page_object(chrome_driver):
     """Тест проверяет работу калькулятора с использованием PageObject."""
+    
     page = CalculatorPage(chrome_driver)
 
     # Шаг 1: Открываем страницу и задаем задержку
@@ -54,27 +40,26 @@ def test_calculator_with_page_object(chrome_driver):
     result = page.get_result()
     assert result == 15, f"Ожидалось 15, но получено {result}"
 
-
-def test_shop_flow_with_page_objects(firefox_driver):
+def test_shop_flow_with_page_objects(chrome_driver): 
     """Полный сценарий покупки товаров в магазине с использованием PageObject."""
     
     # Авторизация
-    login_page = LoginPage(firefox_driver)
+    login_page = LoginPage(chrome_driver)
     login_page.open()
     login_page.enter_credentials('standard_user', 'secret_sauce')
 
     # Покупка товаров
-    shop_page = ShopMainPage(firefox_driver)
+    shop_page = ShopMainPage(chrome_driver)
     shop_page.add_backpack_to_cart()
     shop_page.add_tshirt_to_cart()
     shop_page.add_onesie_to_cart()
     shop_page.go_to_cart()
 
     # Оформление заказа
-    cart_page = ShopCartPage(firefox_driver)
+    cart_page = ShopCartPage(chrome_driver)
     cart_page.proceed_to_checkout()
 
-    checkout_page = ShopCheckoutPage(firefox_driver)
+    checkout_page = ShopCheckoutPage(chrome_driver)
     checkout_page.fill_form('Иван', 'Иванов', '12345')
     checkout_page.continue_to_overview()
 
